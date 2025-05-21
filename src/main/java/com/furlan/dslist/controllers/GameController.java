4e4be294d1304a3ed6e1ebc11397d8c9dd348ea4
Rafael Medeiros Furlan <rafaelfurlan45@yahoo.com>
2025-05-21 14:35:17 -0300

    GET game by id

--- src/main/java/com/furlan/dslist/controllers/GameController.java
+++ src/main/java/com/furlan/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.furlan.dslist.controllers;
 
+import com.furlan.dslist.dto.GameDTO;
 import com.furlan.dslist.dto.GameMinDTO;
 import com.furlan.dslist.entities.Game;
 import com.furlan.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
